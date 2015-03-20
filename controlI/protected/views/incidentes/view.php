a06d3fe1cdf4338a1ce5dc82eddb24647ff2fdc8
diegomart <judimay18@gmail.com>
2015-03-19 23:44:30 -0600

    Borrado de basura

--- controlI/protected/views/incidentes/view.php
+++ controlI/protected/views/incidentes/view.php
@@ -9,24 +9,10 @@ $this->menu=array(
 	array('label'=>'Actualizar Incidente', 'url'=>array('update', 'id'=>$model->idIncidente)),
 	array('label'=>'Eliminar Incidente', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->idIncidente),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Administrar Incidentes', 'url'=>array('admin')),
-<<<<<<< HEAD
-=======
-	array('label'=>'List Incidentes', 'url'=>array('index')),
 	array('label'=>'Create Incidentes', 'url'=>array('create')),
-	array('label'=>'Update Incidentes', 'url'=>array('update', 'id'=>$model->idIncidente)),
-	array('label'=>'Delete Incidentes', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->idIncidente),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Cerrar Incidente', 'url'=>array('cerrar', 'id'=>$model->idIncidente)),
-	array('label'=>'Manage Incidentes', 'url'=>array('admin')),
-	array('label'=>'Reporte de Incidentes', 'url'=>array('pdf')),
-<<<<<<< HEAD
-
-=======
->>>>>>> f739eaa2b633b29d6c7d21feddc7adc1f96e8123
->>>>>>> 522aa625fbc07241bc226d732e314acccfa8aa2c
-=======
 	array('label'=>'Reporte de Incidentes', 'url'=>array('pdf')),
 
->>>>>>> 4063fd9cc20d28384615fd8124788f7138a19d3f
 );
 ?>
 
