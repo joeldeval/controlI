6626365ee062022081f8069cab13a45a7918f37f
= <=>
2015-03-19 23:59:27 -0600

    quitando listado de crear incidente

--- controlI/protected/views/incidentes/create.php
+++ controlI/protected/views/incidentes/create.php
@@ -2,16 +2,7 @@
 /* @var $this IncidentesController */
 /* @var $model Incidentes */
 
-$this->breadcrumbs=array(
-	'Incidentes'=>array('index'),
-	'Create',
-);
 
-$this->menu=array(
-	array('label'=>'Listar Incidentes', 'url'=>array('index')),
-	array('label'=>'Administrar Incidentes', 'url'=>array('admin')),
-	array('label'=>'Reporte de Incidentes', 'url'=>array('pdf')),
-);
 ?>
 
 
