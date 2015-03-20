ac5a81e42dc583f2ebf16088ec8bd6a7bc970c56
= <=>
2015-03-19 23:47:19 -0600

    eliminando desmadres

--- controlI/protected/views/incidentes/view.php
+++ controlI/protected/views/incidentes/view.php
@@ -9,22 +9,6 @@ $this->menu=array(
 	array('label'=>'Actualizar Incidente', 'url'=>array('update', 'id'=>$model->idIncidente)),
 	array('label'=>'Eliminar Incidente', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->idIncidente),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Administrar Incidentes', 'url'=>array('admin')),
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-	array('label'=>'List Incidentes', 'url'=>array('index')),
-=======
->>>>>>> a06d3fe1cdf4338a1ce5dc82eddb24647ff2fdc8
-	array('label'=>'Create Incidentes', 'url'=>array('create')),
-	array('label'=>'Cerrar Incidente', 'url'=>array('cerrar', 'id'=>$model->idIncidente)),
-	array('label'=>'Reporte de Incidentes', 'url'=>array('pdf')),
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 742e6950adc1940369feb8c055e3ffccdcbda8d5
-=======
->>>>>>> a06d3fe1cdf4338a1ce5dc82eddb24647ff2fdc8
 
 );
 ?>
