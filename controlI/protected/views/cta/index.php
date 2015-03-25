b4a2bfd329bf1c7aa359cdb289bd506db6566244
Joel Valdivia <jooelvaldivi@gmail.com>
2015-03-24 22:54:03 -0600

    codigo limpio
    codigo limpio

--- controlI/protected/views/cta/index.php
+++ controlI/protected/views/cta/index.php
@@ -2,11 +2,6 @@
 /* @var $this CtaController */
 /* @var $dataProvider CActiveDataProvider */
 
-$this->breadcrumbs=array(
-<<<<<<< HEAD
-	'Usuarios',
-);
-
 $this->menu=array(
 	array('label'=>'Administrar Usuarios', 'url'=>array('admin')),
 );
@@ -14,18 +9,6 @@ $this->menu=array(
 <div id="form-main">
   <div id="form-div">
 <h1>Usuarios de CTA</h1>
-=======
-	'Cta',
-);
-
-$this->menu=array(
-	array('label'=>'Crear Usuario', 'url'=>array('create')),
-	array('label'=>'Administrar Usuarios', 'url'=>array('admin')),
-);
-?>
-
-<h1>Cta</h1>
->>>>>>> 39ecd1651a763f6a242cc89bbd32e22ec3ed66d8
 
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
