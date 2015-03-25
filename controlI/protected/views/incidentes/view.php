85b7f472c5047f90750be6114f35cff8727fc298
= <=>
2015-03-24 23:32:21 -0600

    diseño de la vista

--- controlI/protected/views/incidentes/view.php
+++ controlI/protected/views/incidentes/view.php
@@ -19,6 +19,7 @@ $this->menu=array(
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
+	'itemCssClass'=>'table table-striped table-hover  table-condensed',
 	'attributes'=>array(
 		'idIncidente',
 		'Descripcion',
