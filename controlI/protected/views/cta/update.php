45e9bca14615495bde5357d18b81f33b02efca25
Joel Valdivia <jooelvaldivi@gmail.com>
2015-03-24 22:54:33 -0600

    codigo limpio
    codigo limpio

--- controlI/protected/views/cta/update.php
+++ controlI/protected/views/cta/update.php
@@ -2,7 +2,7 @@
 /* @var $this CtaController */
 /* @var $model Cta */
 
-<<<<<<< HEAD
+
 
 $this->menu=array(
 	array('label'=>'Detalles Usuario', 'url'=>array('view', 'id'=>$model->idCta)),
@@ -12,22 +12,5 @@ $this->menu=array(
 <div id="form-main">
   <div id="form-div">
 <h1>Actualizar Usuario <?php echo $model->idCta; ?></h1>
-=======
-$this->breadcrumbs=array(
-	'Cta'=>array('index'),
-	$model->idCta=>array('view','id'=>$model->idCta),
-	'Actualizar',
-);
-
-$this->menu=array(
-	array('label'=>'Listar Usuarios', 'url'=>array('index')),
-	array('label'=>'Crear Usuario', 'url'=>array('create')),
-	array('label'=>'Ver Usuario', 'url'=>array('view', 'id'=>$model->idCta)),
-	array('label'=>'Administrar Usuarios', 'url'=>array('admin')),
-);
-?>
-
-<h1>Actualizar Usuario Cta <?php echo $model->idCta; ?></h1>
->>>>>>> 39ecd1651a763f6a242cc89bbd32e22ec3ed66d8
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
